1737da8a585eef3d4621773360b0959d509510f6
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-09-17 17:38:22 +0100

    Get dropbox working

---
+++ lib/googleDriveWrapper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Googledrivewrapper extends StatefulWidget {
+  const Googledrivewrapper({super.key});
+
+  @override
+  State<Googledrivewrapper> createState() => _GoogledrivewrapperState();
+}
+
+class _GoogledrivewrapperState extends State<Googledrivewrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
