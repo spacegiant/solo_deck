a808103f2ededd17f23182c3cc3e633f71d9f7b0
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-10-21 11:19:49 +0100

    Reset to base project as abandoning Dropbox integration for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:untitled/dropboxWrapper.dart';
-
-enum CloudProvider { dropbox, google, localOnly }
-
 void main() async {
-  await dotenv.load(fileName: ".env");
   return runApp(const MyApp());
 }
 
@@ -34,6 +28,6 @@ class HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return const DropboxWrapper();
+    return const Text('APP');
   }
 }
