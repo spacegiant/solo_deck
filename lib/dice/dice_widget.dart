d8ad33b838e541463dd39bf52622951e84d49d71
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-10-24 19:20:30 +0100

    Add polyhedral 8

--- lib/dice/dice_widget.dart
+++ lib/dice/dice_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:SoloDeck/dice/polyhedrals/inject_colours_into_SVG.dart';
+import 'package:SoloDeck/dice/polyhedrals/polyhedral_eight.dart';
 import 'package:SoloDeck/dice/polyhedrals/polyhedral_five.dart';
 import 'package:SoloDeck/dice/polyhedrals/polyhedral_four.dart';
 import 'package:SoloDeck/dice/polyhedrals/polyhedral_seven.dart';
@@ -13,13 +14,13 @@ import '../utils/convert_color_to_hex.dart';
 class SVGString {}
 
 SvgPicture diceWidget({
-  Color color = const Color(0xFFFFC0CB),
-  Color highlightColor = const Color(0xFFFFFF00),
+  Color color = const Color(0xFF88636A),
+  Color highlightColor = const Color(0xFFCC98A2),
   double width = 60,
 }) {
   return SvgPicture.string(
     injectColoursIntoSVG(
-        p7, convertColorToHex(color), convertColorToHex(highlightColor)),
+        p8, convertColorToHex(color), convertColorToHex(highlightColor)),
     width: width,
     height: width,
   );
